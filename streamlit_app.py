bc6955840c8a510c73aad8957fc69ace66188277
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2024-02-29 14:59:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+#import joblib
 
-st.write('Hello world!')
+# Load the model
+model = pickle.load('nlp
+#model = joblib.load('text_clf_model.joblib')
+
+# Function to predict text and get score
+def predict_text_with_score(model, text):
+    prediction = model.predict([text])
+    prediction_proba = model.predict_proba([text])
+    class_labels = model.classes_
+    proba_scores = {class_labels[i]: prediction_proba[0][i] for i in range(len(class_labels))}
+    return prediction[0], proba_scores
+
+# Streamlit user interface
+st.title('Text Classification App')
+user_input = st.text_area("Enter text here:")
+if st.button('Predict'):
+    prediction, scores = predict_text_with_score(model, user_input)
+    st.write("Predicted class:", prediction)
+    st.write("Probability Scores:", scores)
