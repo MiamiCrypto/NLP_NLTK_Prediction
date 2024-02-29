3c1f47eb150b6b8f6508c5afd52c57d1e2a40093
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2024-02-29 15:35:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def predict_text_with_score(model, text):
 
 # Streamlit user interface
 st.title('Text Prediction App')
-st.image("Thinking Brain.png")
+st.image("Thinking Brain.png", caption='???', width=250)
 user_input = st.text_area("Enter text here:")
 if st.button('Predict'):
     prediction, scores = predict_text_with_score(model, user_input)
